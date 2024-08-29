8ee8fe8a3c4c4a45edae79147d4eb69a7463ab77
Luiz Felipe Santana Dos Santos <fs07santana@gmail.com>
2024-08-28 21:30:34 -0300

    Create UserRegisterDTO.cs

---
+++ Domain/DTOs/UserRegisterDTO.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coin_api.Domain.DTOs
+{
+    public class UserRegisterDTO
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        // public string SenhaForte { get; set; } // = string.Empty;
+    }
+}
