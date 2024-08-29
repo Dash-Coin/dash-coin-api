b676c5714b41ec031fca545120108ce416ad6662
Luiz Felipe Santana Dos Santos <fs07santana@gmail.com>
2024-08-28 21:29:33 -0300

    create userController

--- Controller/UserController.cs
+++ Controller/UserController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using coin_api.Application.Service;
 using coin_api.Application.ViewModel;
+using coin_api.Domain.DTOs;
 using coin_api.Domain.Model;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -15,30 +17,99 @@ namespace coin_api.Controller
     [Route("api/v1/user")]
     public class UserController : ControllerBase
     {
-        private readonly IUserRepository _userRepository;
+        private readonly UserService _userService;
 
-        public UserController(IUserRepository userRepository)
+        public UserController(UserService userService)
         {
-            _userRepository = userRepository ?? throw new ArgumentNullException();
+            _userService = userService;
         }
 
         [Authorize]
-        [HttpPost]
-        public IActionResult Add(UserViewModel userView)
+        [HttpPost("registrar")]
+        public async Task<IActionResult> Registrar(UserRegisterDTO request)
         {
-            var user = new User(userView.Name, userView.Email);
-            _userRepository.Add(user);
+            if (await _userService.UserExists(request.Email))
+                return BadRequest("Usuário já existe");
 
-            return Ok();
+            UserService.CriarSenhaHash(request.Password, out byte[] senhaHash, out byte[] senha);
+
+            var user = new User
+            {
+                email = request.Email,
+                senhaHash = senhaHash,
+                senha = senha,
+                token = UserService.CriarToken()
+            };
+
+            await _userService.CreateUser(user);
+
+            return Ok("Usuário criado com sucesso");
         }
 
         [Authorize]
-        [HttpGet]
-        public IActionResult Get()
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserLoginDTO request)
         {
-            var users = _userRepository.Get();
+            var user = await _userService.Authenticate(request.Email, request.Password);
+            if (user == null)
+                return BadRequest("Email ou senha incorretos.");
 
-            return Ok(users);
+            var refreshToken = UserService.CriarToken();
+
+            var userLogged = new UserLoginResponse(user, refreshToken);
+
+            return Ok(userLogged);
+        }
+
+        // verifica se token ativo ainda é valido
+        [HttpPost("verificar")]
+        public async Task<IActionResult> Verificar(string token)
+        {
+            var user = await _userService.GetUserByToken(token);
+            if (user == null)
+                return BadRequest("Token inválido.");
+
+            user.expiraToken = DateTime.Now;
+            await _userService.SaveChanges();
+
+            return Ok("Usuário verificado.");
+        }
+
+        // [HttpPost("alterar-senha")]
+        // public async Task<IActionResult> EsquecerSenha(UserLoginDTO request)
+        // {
+        //     var user = await _userService.GetUserByEmail(request.Email);
+        //     if (user == null)
+        //         return BadRequest("Usuário não encontrado.");
+
+        //     // user.reseteSenha = UserService.CriarToken();
+        //     user.expiraToken = DateTime.Now.AddDays(5);
+
+        //     await _userService.SaveChanges();
+
+        //     return Ok("Senha alterada");
+        // }
+
+        [HttpPost("alterar-senha")]
+        public async Task<IActionResult> ResetarSenha(ResetPasswordDTO request)
+        {
+            var user = await _userService.GetUserByEmail(request.Email);
+            if (user == null)
+                return BadRequest("Usuário não encontrado.");
+
+            // var userToken = await _userService.GetUserByToken(request.Token);
+            if (user.token == null || user.expiraToken < DateTime.Now)
+                return BadRequest("Token inválido.");
+
+            UserService.CriarSenhaHash(request.NewPassword, out byte[] senhaHash, out byte[] senha);
+            user.senhaHash = senhaHash;
+            user.senha = senha;
+            // user.reseteSenha = null;
+            // user.expiraToken = null;
+
+            await _userService.SaveChanges();
+
+            return Ok("Senha alterada com sucesso.");
         }
     }
 }
