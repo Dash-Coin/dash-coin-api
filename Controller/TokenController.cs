8515a7f702ac9d559f20ebada5a613e17bb03fc1
Luiz Felipe Santana Dos Santos <fs07santana@gmail.com>
2024-08-28 21:30:55 -0300

    Update TokenController.cs

--- Controller/TokenController.cs
+++ Controller/TokenController.cs
@@ -1,3 +1,4 @@
+using coin_api.Application.Service;
 using coin_api.Domain.DTOs;
 using coin_api.Token;
 using Microsoft.AspNetCore.Authorization;
@@ -26,9 +27,9 @@ namespace coin_api.Controller
             if (string.IsNullOrWhiteSpace(Input.Email) || string.IsNullOrWhiteSpace(Input.Password))
                 return Unauthorized();
 
-            var user = await _userService.PasswordSignInAsync(Input.Email, Input.Password, false, lockoutOnFailure: false);
+            var user = await _userService.Authenticate(Input.Email, Input.Password);
 
-            if (user.Succeeded) 
+            if (user != null) 
             {
                 var token = new TokenJwtBuilder()
                     .AddSecurityKey(JwtSecurityKey.Create("JGHF4W3KHUG2867RUYFSDUIYFDT%DBHAJHKSFFY%"))
