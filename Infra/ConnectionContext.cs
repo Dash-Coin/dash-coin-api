490181969ea9c943133e692d66a86a1f4704d648
Luiz Felipe Santana Dos Santos <fs07santana@gmail.com>
2024-08-31 19:43:50 -0300

    update connection context

--- Infra/ConnectionContext.cs
+++ Infra/ConnectionContext.cs
@@ -9,11 +9,22 @@ public class ConnectionContext : DbContext
 
     public DbSet<Transaction> Transactions { get; set; }
     public DbSet<User> Users { get; set; }
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    => optionsBuilder.UseNpgsql(
-              "Server=localhost;" +
-              "Port=5433;Database=coinapi;" +
-              "User Id=user;" +
-              "Password=123;");
+    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    // => optionsBuilder.UseNpgsql(
+    //           "Server=localhost;" +
+    //           "Port=5433;Database=coinapi;" +
+    //           "User Id=user;" +
+    //           "Password=123;");
+
+    public ConnectionContext(DbContextOptions<ConnectionContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.ToTable("users"); // Certifique-se de que o nome da tabela esteja correto
+        });
+
+    }
 
 }
--- Program.cs
+++ Program.cs
@@ -1,18 +1,59 @@
 using System.IdentityModel;
 using System.Text;
 using coin_api;
+using coin_api.Application.Service;
+using coin_api.Controller;
 using coin_api.Domain.Model;
+using coin_api.Token;
 using Microsoft.AspNetCore.Authentication.BearerToken;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<ConnectionContext>(options => 
+         options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                  .AddJwtBearer(option =>
+                  {
+                     option.TokenValidationParameters = new TokenValidationParameters
+                     {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+
+                        ValidIssuer = "Teste.Securiry.Bearer",
+                        ValidAudience = "Teste.Securiry.Bearer",
+                        IssuerSigningKey = JwtSecurityKey.Create("JGHF4W3KHUG2867RUYFSDUIYFDT%DBHAJHKSFFY%")
+                     };
+
+                     option.Events = new JwtBearerEvents
+                     {
+                        OnAuthenticationFailed = context =>
+                        {
+                           Console.WriteLine("OnAuthenticationFailed: " + context.Exception.Message);
+                           return Task.CompletedTask;
+                        },
+                        OnTokenValidated = context =>
+                        {
+                           Console.WriteLine("OnTokenValidated: " + context.SecurityToken);
+                           return Task.CompletedTask;
+                        }
+                     };
+                  });
+
 builder.Services.AddScoped<TransactionRepository>();
+builder.Services.AddScoped<UserService>();
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapGet("/", () => "Hello World!");
 
