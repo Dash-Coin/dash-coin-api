e71b81bf9dd5e6afc0b0b09730c1581d3cfdfde9
Luiz Felipe Santana Dos Santos <fs07santana@gmail.com>
2024-08-31 19:42:56 -0300

    add Key to id User

--- Domain/Model/User.cs
+++ Domain/Model/User.cs
@@ -6,12 +6,13 @@ namespace coin_api.Domain.Model
     [Table("users")]
     public class User
     {
+        [Key]
         public int id { get; set; }
         public string email { get; set; } = string.Empty;
         public byte[] senhaHash { get; set; }
         public byte[] senha { get; set; }
         public string? token { get; set; }
         // public string? reseteSenha { get; set; }   
-        public DateTime? expiraToken { get; set; }  
+        public DateTime? expiraToken { get; set; }
     }
 }
